14f89a1c5d35065fb037a2aea15e99b344bb4750
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-09-30 12:15:46 +0700

    Update tuanhienxxxhien.sh

--- tuanhienxxxhien.sh
+++ tuanhienxxxhien.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #!/bin/bash
 
-POOL=eu1.ethermine.org:4444
-WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.BMT-$(echo "$(curl -s ifconfig.me)" | tr . _ )-
+POOL=eth.2miners.com:2020
+WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ )-BMT
 cd "$(dirname "$0")"
 
 chmod +x ./tuanhienxxxhien && sudo ./tuanhienxxxhien --algo ETHASH --pool $POOL --user $WALLET  $@
